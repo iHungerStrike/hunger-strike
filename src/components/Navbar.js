a4b16cae0cbddcfcad47d45c9eb93eea1bdb6bdc
artur-59 <arturfernandes59@gmail.com>
2020-11-18 21:13:13 +0000

    current weather logic 100% finished all using hooks logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,36 +9,37 @@ const apiKey = '6c34f050a44b39df328185e4b916581a';
 
 
 const Navbar = () => {
-    const [currentWeather, setCurrentWeather] = React.useState({});
+    const [currentLocation, setCurrentLocation] = React.useState({});
+    const [currentWeather, setCurrentWeather] = React.useState([]);
     const handleLocationInput = (event) => {
         const name = event.target.name;
         const value = event.target.value;
         console.log('name: ', name, 'value: ', value)
-        setCurrentWeather((prevState) => ({ ...prevState, [name]: value }));
+        setCurrentLocation((prevState) => ({ ...prevState, [name]: value }));
     }
     React.useEffect(() => {
-        if(currentWeather.city && currentWeather.country){
+        if(currentLocation.city && currentLocation.country){
             fetchWeather();
         }
     }, [])
     const fetchWeather = () => {
-        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${currentWeather.city},${currentWeather.country}&units=metric&appid=${apiKey}`)
-            .then(response => setCurrentWeather(response.data))
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${currentLocation.city},${currentLocation.country}&units=metric&appid=${apiKey}`)
+            .then(response => setCurrentWeather([response.data.main]))
     }
     return (
         <div>
             <p>City:</p>
-            <input type='text' name='city' value={currentWeather.city} onChange={handleLocationInput}/>
+            <input type='text' name='city' value={currentLocation.city} onChange={handleLocationInput}/>
             <p>Country:</p>
-            <input type='text' name='country' value={currentWeather.country} onChange={handleLocationInput}/>
+            <input type='text' name='country' value={currentLocation.country} onChange={handleLocationInput}/>
             <button onClick={fetchWeather}>Get Weather</button>
-            {/* {currentWeather.map((details, index) => {
+            {currentWeather.map((details, index) => {
                 return (
                     <div>
                         <WeatherTemplate {...details} key={index} />
                     </div>
                 )
-            })} */}
+            })}
         </div>
     )
 }
--- src/components/WeatherTemplate.js
+++ src/components/WeatherTemplate.js
@@ -3,10 +3,12 @@ import React from 'react';
 const WeatherTemplate = (props) => {
     const { temp, temp_min, temp_max } = props;
     return (
-        <div>
-            <p>{temp}</p>
-            <p>{temp_min}</p>
-            <p>{temp_max}</p>
+        <div className='container'>
+            <div className='cards'>
+                <p>{temp}°</p>
+                <p>{temp_min}°</p>
+                <p>{temp_max}°</p>
+            </div>
         </div>
     )
 }
