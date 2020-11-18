dd5f8c60a0d643a012d4fb728d08b4b0707c61a8
artur-59 <arturfernandes59@gmail.com>
2020-11-18 23:53:27 +0000

    realized I was missing the description and icon ID which are important to render the proper icon. Need to work on the switch statement to render the correct icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,17 +14,53 @@ const Navbar = () => {
     const handleLocationInput = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        console.log('name: ', name, 'value: ', value)
+        // console.log('name: ', name, 'value: ', value)
         setCurrentLocation((prevState) => ({ ...prevState, [name]: value }));
     }
     React.useEffect(() => {
         if(currentLocation.city && currentLocation.country){
             fetchWeather();
         }
-    }, [])
+    }, [currentLocation])
     const fetchWeather = () => {
         axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${currentLocation.city},${currentLocation.country}&units=metric&appid=${apiKey}`)
-            .then(response => setCurrentWeather([response.data.main]))
+            .then(response => {
+                let temperature = response.data.main;
+                let weather = response.data.weather[0];
+                let sweaterWeater = Object.assign(temperature, weather);
+                setCurrentWeather([sweaterWeater]);
+                // let rangeID = sweaterWeater.id;
+                // get_WeatherIcon(icons, rangeId) {
+                //     switch (true) {
+                //       case rangeId >= 200 && rangeId < 232:
+                //         this.setState({ icon: icons.Thunderstorm });
+                //         break;
+                //       case rangeId >= 300 && rangeId <= 321:
+                //         this.setState({ icon: icons.Drizzle });
+                //         break;
+                //       case rangeId >= 500 && rangeId <= 521:
+                //         this.setState({ icon: icons.Rain });
+                //         break;
+                //       case rangeId >= 600 && rangeId <= 622:
+                //         this.setState({ icon: icons.Snow });
+                //         break;
+                //       case rangeId >= 701 && rangeId <= 781:
+                //         this.setState({ icon: icons.Atmosphere });
+                //         break;
+                //       case rangeId === 800:
+                //         this.setState({ icon: icons.Clear });
+                //         break;
+                //       case rangeId >= 801 && rangeId <= 804:
+                //         this.setState({ icon: icons.Clouds });
+                //         break;
+                //       default:
+                //         this.setState({ icon: icons.Clouds });
+                //     }
+                //   }
+            })
+            .catch((error) => {
+                console.error(error.response.data);
+              });
     }
     return (
         <div>
@@ -36,7 +72,7 @@ const Navbar = () => {
             {currentWeather.map((details, index) => {
                 return (
                     <div>
-                        <WeatherTemplate {...details} key={index} />
+                        <WeatherTemplate {...details} key={index} cityName={currentLocation.city}/>
                     </div>
                 )
             })}
--- src/components/WeatherTemplate.js
+++ src/components/WeatherTemplate.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 const WeatherTemplate = (props) => {
-    const { temp, temp_min, temp_max } = props;
+    const { temp, temp_min, temp_max, cityName, description, id } = props;
     return (
         <div className='container'>
             <div className='cards'>
-                <p>{temp}°</p>
-                <p>{temp_min}°</p>
-                <p>{temp_max}°</p>
+                <h1>{cityName}</h1>
+                <h5 className='py-4'>
+                    Weather Icon
+                </h5>
+                <h1 className='py-2'>{temp}&deg;</h1>
+                <h3>
+                    <span className='px-4'>{temp_min}&deg;</span>
+                    <span className='px-4'>{temp_max}&deg;</span>
+                </h3>
+                <h4 className='py-3'>{description.charAt(0).toUpperCase() + description.slice(1)}</h4>
+                <h1>Background Image ID: {id}</h1>
             </div>
         </div>
     )
